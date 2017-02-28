fe9712d873c37cc8d73f65a7f9d8b2e52d820307
zhouzhiqiang <mumu@appadhoc.com>
2017-02-28 10:49:12 +0800

    add MatchTest

--- src/main/scala-2.11/CollectionTest.scala
+++ src/main/scala-2.11/CollectionTest.scala
@@ -2,6 +2,26 @@ import scala.collection.mutable
 
 object CollectionTest extends App{
 
+  val Seq(n, m) = Seq(3,2)
+  //  println(s"n,m: $n,$m")
+
+  val mapSeq = Seq(Map("a" -> 1, "b" -> "str"))
+  val Seq(values)
+//  val Seq(aV, bV)
+    = mapSeq.map { record =>
+    (
+      record.getOrElse("a", 0).asInstanceOf[Int],
+      record.getOrElse("b", "").asInstanceOf[String]
+    )
+  }
+  println(s"values: $values")
+
+  /** MatchError:*/
+//  println(s"aV: $aV, $bV")
+
+
+
+
   val map1 = mutable.Map[Int, String]()
   map1 += (1 -> "a")
   map1 += (2 -> "b")
@@ -15,16 +35,16 @@ object CollectionTest extends App{
   val m1 = (1 to 3).flatMap { n =>
     addToMap(n, immutableMap)
   }.toMap
-  m1.foreach(println)
+//  m1.foreach(println)
 
   val m2= (4 to 6).flatMap { n =>
     addToMap(n, immutableMap)
   }.toMap
-  m2.foreach(println)
-  println("********")
+//  m2.foreach(println)
+//  println("********")
 
   val res = m1 ++ m2
-  res.foreach(println)
+//  res.foreach(println)
 
   val mutableMap = scala.collection.mutable.Map[String, Int]()
   def addToMap(map: mutable.Map[String, Int]): Map[String, Int] = {
